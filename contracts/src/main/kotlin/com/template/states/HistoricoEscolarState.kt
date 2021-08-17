802d558f50b658274cf5f65eae07cde75917f926
Juan Minango <jcarlosminango@gmail.com>
2021-08-17 15:46:17 -0300

    CriarHistoricoEscolarFlow test class

--- contracts/src/main/kotlin/com/template/states/HistoricoEscolarState.kt
+++ contracts/src/main/kotlin/com/template/states/HistoricoEscolarState.kt
@@ -1,6 +1,8 @@
 package com.template.states
 
+import com.template.contracts.HistoricoEscolarContract
 import com.template.model.HistoricoEscolar
+import net.corda.core.contracts.BelongsToContract
 import net.corda.core.contracts.LinearState
 import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.identity.AbstractParty
@@ -8,6 +10,7 @@ import net.corda.core.identity.Party
 import net.corda.core.serialization.CordaSerializable
 
 @CordaSerializable
+@BelongsToContract(HistoricoEscolarContract::class)
 data class HistoricoEscolarState(
     val historicoEscolar: HistoricoEscolar,
     val faculdadesReceptoras: List<Party> = listOf(),
